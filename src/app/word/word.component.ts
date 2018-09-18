934b5ede5e5a72c0ff8ffe4a11bf920e6d488277
anhquoctran <aquoc.hue@outlook.com>
2018-09-18 16:20:48 +0700

    initialize project

---
+++ src/app/word/word.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: './word.component.html',
+    selector: 'app-word',
+    styleUrls: [
+        './word.component.css'
+    ]
+})
+
+export class WordComponent implements OnInit {
+    constructor() { }
+    name = '';
+    ngOnInit() {}
+
+}
